4eabbd0cdfdffb4554f9f48fb100eae620047101
omolaso <adextee001@gmail.com>
2022-07-15 16:56:21 +0100

    'Brightly'

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ menuIcon.addEventListener('click', () => {
     const nav = document.querySelector('.nav1');
     console.log(nav)
     nav.classList.toggle('list');
-});
+    const body = document.querySelector('.body');
+    body.classList.toggle('body_margin')
+})
+
